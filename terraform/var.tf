4cf64c6bbaeb0b48cd7ada2a3b83191c4aa2b870
Priti <61485216+pritiranga@users.noreply.github.com>
2022-12-09 12:28:20 +0530

    Create var.tf

---
+++ terraform/var.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = "eastus"
+}
+
+variable "rg" {
+    default = "app-service-demo"
+}
+
+variable "appserviceplanname" {
+    default = "asp1001"
+}
+
+variable "webappname" {
+    default = "webapp-1001"
+}
+
