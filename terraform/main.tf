ac5adde4b9eed91f5b41e3ccaac097ae14cc5cd8
Priti <61485216+pritiranga@users.noreply.github.com>
2022-12-09 12:27:54 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.region
+}
+
+# Create App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = var.appserviceplanname
+  location            = var.region
+  resource_group_name = var.rg
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = var.webappname
+  location              = var.region
+  resource_group_name   = var.rg
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+}
+
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
+}
